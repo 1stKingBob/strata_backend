66363b0b2fcca89870bcf6ea51f6f8f41c01766d
1stKingBob <nevanntanwar@gmail.com>
2025-06-02 17:01:56 +1000

    Update get_user_levy.php

--- get_user_levy.php
+++ get_user_levy.php
@@ -1,8 +1,9 @@
 <?php
-$host = "your-db-host";
-$dbname = "strata_cnw3";
-$user = "your-db-user";
-$pass = "your-db-pass";
+$host = getenv("DB_HOST");
+$port = getenv("DB_PORT") ?: "5432";
+$dbname = getenv("DB_NAME");
+$user = getenv("DB_USER");
+$pass = getenv("DB_PASS");
 
 $conn = new PDO("pgsql:host=$host;dbname=$dbname", $user, $pass);
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
